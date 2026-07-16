f0f74f06a0b2456955b1223a41e2a0dee000e163
Sil van Diepen <me@sil.mt>
2026-07-16 16:46:38 +0200

    build: add Swift package foundation

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "ImageKid",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "ImageKid", targets: ["ImageKid"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "ImageKid",
+            path: "Sources/ImageKid"
+        ),
+        .testTarget(
+            name: "ImageKidTests",
+            dependencies: ["ImageKid"],
+            path: "Tests/ImageKidTests"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
